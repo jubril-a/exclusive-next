c3cc7e1cd7e84416b7d6a5b525d551eada318459
Abdulsalam <abdulsalamjubr3@gmail.com>
2025-11-20 15:06:07 +0200

    .

--- app/account/cart/Cart.tsx
+++ app/account/cart/Cart.tsx
@@ -1,3 +1,4 @@
+import { client } from "@/utils/sanityClient"
 import CartClient from "./CartClient"
 import { createClient } from "@/utils/supabase/server"
 
@@ -7,7 +8,31 @@ const Cart = async () => {
   .from('cart')
   .select('product_id')
 
-  return <CartClient data={data} />
+  if (data?.length) {
+    const ids = data?.map((item) => `_id == "${item.product_id}"`) || []
+    const getProduct = async () => {
+    
+      const query = `
+        *[_type == "product"][${ids.join(" || ")}]{
+          _id,
+          productName,
+          price,
+          discount,
+          "imageUrl": productImage.asset->url,
+          stock,
+        }`
+
+      const data = await client.fetch(query)
+      return data
+    }
+    
+    const productsData = await getProduct()
+
+    return <CartClient data={productsData} />
+  } else {
+    return <CartClient data={[]} />
+  }
+
 }
 
 export default Cart
--- app/account/cart/components/CartItem.tsx
+++ app/account/cart/components/CartItem.tsx
@@ -1,29 +1,48 @@
-import { client } from "@/utils/sanityClient"
-import CartItemClient from "./ItemClient"
+import Button from "@/components/Button"
+import Counter from "@/components/Counter"
+import { removeFromCart } from "@/utils/data/supabase"
+import { useState } from "react"
+import { Dispatch, SetStateAction } from "react"
 
-const CartItem = async ({id}: {id: string}) => {
-
-    const getProduct = async () => {
-
-      const query = `
-        *[_type == "product"][_id == "${id}"]{
-          _id,
-          productName,
-          price,
-          discount,
-          "imageUrl": productImage.asset->url,
-          stock,
-        }`
-
-      const data = await client.fetch(query)
-      return data[0]
-
-    }
-
-    const productData = await getProduct()
-
-  return <CartItemClient {...productData} />
+type Props = {
+  _id: string,
+  productName: string,
+  imageUrl: string,
+  price: number,
+  discount: number,
+  stock: number,
+  reload: [boolean, Dispatch<SetStateAction<boolean>>]
 }
 
-export default CartItem
+const ItemClient = ({ _id, productName, imageUrl, price, discount, stock, reload }: Props) => {
 
+  const [isInCart, setInCart] = useState(true)
+  // const [state, changeState] = reload
+
+  function handleClick() {
+    const response = removeFromCart(_id)
+    setInCart(false)
+    // console.log(state)
+    // changeState((state) => !state)
+    // console.log(state)
+  }
+
+  return (
+    isInCart &&
+    <div className="py-4 border-b border-gray-100">
+      <div className="min-[480px]:flex min-[480px]:justify-between items-center mb-4 min-[480px]:gap-8">
+        <div className="flex gap-4 max-[480px]:mb-4 items-center">
+          <img className="h-[80px]" src={imageUrl} alt="" />
+          <h2 className="max-w-[400px] line-clamp-2">{productName}</h2>
+        </div>
+        <span className="text-xl font-semibold">₦{(price - (price * (discount / 100))).toLocaleString('en-US')}</span>
+      </div>
+      <div className="flex max-[360px]:flex-col min-[360px]:justify-between min-[360px]:items-center gap-4">
+        <Button clickHandler={handleClick}>Remove</Button>
+        <Counter className="order-[-1]" stock={stock}/>
+      </div>
+    </div>
+  )
+}
+
+export default ItemClient
